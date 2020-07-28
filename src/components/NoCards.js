c0b81a3ca54a4befad9f8262f2350e31ea82aa0d
Ivan Cheng <ivan@ivancheng.co.uk>
2020-07-28 18:48:16 +0100

    noCards to stateless

--- src/components/NoCards.js
+++ src/components/NoCards.js
@@ -1,31 +1,15 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React from "react";
 import { Text, View, TouchableOpacity } from "react-native";
 import { myStyles } from "../utils/myStyles";
 
-function mapStateToProps(state, { title, id, addCard }) {
-  return {
-    title,
-    id,
-    addCard,
-  };
-}
+const NoCards = ({ title, id, addCard }) => (
+  <View style={myStyles.container}>
+    <Text style={myStyles.title}>{title}</Text>
+    <Text style={myStyles.subtitle}>There are no cards for this Deck</Text>
+    <TouchableOpacity style={myStyles.btn} onPress={() => addCard(id)}>
+      <Text style={myStyles.btnText}>add card</Text>
+    </TouchableOpacity>
+  </View>
+);
 
-class NoCards extends Component {
-  render() {
-    return (
-      <View style={myStyles.container}>
-        <Text style={myStyles.title}>{this.props.title}</Text>
-        <Text style={myStyles.subtitle}>There are no cards for this Deck</Text>
-        <TouchableOpacity
-          style={myStyles.btn}
-          onPress={() => this.props.addCard(this.props.id)}
-        >
-          <Text style={myStyles.btnText}>add card</Text>
-        </TouchableOpacity>
-      </View>
-    );
-  }
-}
-
-export default connect(mapStateToProps)(NoCards);
+export default NoCards;
