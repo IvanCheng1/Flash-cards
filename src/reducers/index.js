77c0eef40cbd839d36b82f39a925cbfeefd0f0bf
Ivan Cheng <ivan@ivancheng.co.uk>
2020-07-29 09:42:54 +0100

    clean up

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from "redux";
 import decks from "./decks";
-import questions from "./questions"
 import { loadingBarReducer } from "react-redux-loading";
 
 export default combineReducers({
   decks,
-  // questions,
   loadingBarReducer,
 });
